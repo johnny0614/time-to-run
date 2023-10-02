04eaf3a40dbf0946a31badb5ce1eef85ac93cf37
unknown <johnny@savv-e.com.au>
2023-10-02 17:02:29 +1100

    refactor(*) migrate to vite commit 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { render } from "react-snapshot";
+import { render } from "react-dom";
 
 import Root from "./components/Root";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 render(
   <React.StrictMode>
@@ -14,4 +14,4 @@ render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr' 
+
+export default defineConfig({
+  base: '/',
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgr(),
+  ]
+})
